393ec5f7703b38f9a287706b7c7ee9fd43a9f7aa
jolaouille <cle.chec@laposte.net>
2019-10-20 20:06:18 +0200

    ajout lecture fichier wip

--- source.cpp
+++ source.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 
 
 
 
-
+void lireFichier(){
+  std::ifstream monFichier("figure.txt");
+  if (monFichier.is_open()) {
+    std::string line;
+    while (std::getline(monFichier, line)) {
+      std::cout << line << '\n';
+    }
+    monFichier.close();
+  }
+  else {
+    std::cerr << "Unable to open file\n";
+  }
+}
 
 int main()
 {
+  lireFichier();
   std::cout << "Hello Word !!!" << '\n';
   return 0;
 }
