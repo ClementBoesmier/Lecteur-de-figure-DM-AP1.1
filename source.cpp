bdd78dfbdbc85cdf841807ce2017e2d19dc4a0fc
jolaouille <cle.chec@laposte.net>
2019-10-20 21:07:36 +0200

    lecture ficher FINI

--- source.cpp
+++ source.cpp
@@ -6,17 +6,28 @@
 
 
 
-void lireFichier(){
+void lireFichier()
+{
   std::ifstream monFichier("figure.txt");
-  if (monFichier.is_open()) {
-    std::string line;
-    while (std::getline(monFichier, line)) {
-      std::cout << line << '\n';
-    }
-    monFichier.close();
-  }
-  else {
-    std::cerr << "Unable to open file\n";
+  std::string laForme, leRemplissage;
+  int laTaille, laDecal;
+  char leCar;
+  while (not monFichier.eof())
+  {
+    getline(monFichier, laForme);
+    monFichier >> laTaille;
+    monFichier.ignore();
+
+    monFichier >> laDecal;
+    monFichier.ignore();
+
+    monFichier >> leCar;
+    monFichier.ignore();
+
+    getline(monFichier, leRemplissage);
+
+    std::cout << "leCar : " << leCar << '\n';
+    std::cout << "laForme : " << laForme << '\n';
   }
 }
 
