dc9dd02ec1a3b96ea80ef534579a857c35406962
joladouille <cle.chec@laposte.net>
2019-10-24 16:13:43 +0200

    commentaire et psedo optimisation Fini

--- source.cpp
+++ source.cpp
@@ -1,5 +1,5 @@
 //Clement Boesmier
-//ce programe en ligne de commande lit un fichier "figure.txt" situer dans la racine du projet
+//ce programme en ligne de commande lit un fichier "figure.txt" situer dans la racine du projet
 //il dessine les figure géométrique décrite a l'interieur sous le format :
 /*
 nom de la forme (string)
@@ -74,15 +74,15 @@ void dessinerTriangleRectangle(int uneTaille, int unEspace, char unCar, std::str
   {
     dessinerEspace(unEspace);//finction d'espace
 
-    for (int leJ = 0; leJ < leSommai; leJ++)
+    for (int leJ = 0; leJ < leSommai; leJ++)//boucle de hauteur
     {
-      if (unRempl == "fill")
+      if (unRempl == "fill")//condition de remplissage
       {
         std::cout << unCar;
       }
       else
       {
-        if (leJ == 0 || leJ == (leSommai - 1) || leI == (uneTaille - 1))
+        if (leJ == 0 || leJ == (leSommai - 1) || leI == (uneTaille - 1))//condition de contour
         {
           std::cout << unCar;
         }
@@ -93,20 +93,18 @@ void dessinerTriangleRectangle(int uneTaille, int unEspace, char unCar, std::str
       }
     }
     std::cout <<'\n';
-    leSommai++;
+    leSommai++;//incrépent du sommaie pour arrivé a la base
   }
 }
 
+// fonction qui dessine un triangle rectangle inverser
+//elle prend les même argument que dessinerCarre
 void dessinerTriangleRectangleInv(int uneTaille, int unEspace, char unCar, std::string unRempl)
 {
-  int leSommai = uneTaille;
-  for (int leI = 0; leI < uneTaille; leI++)
+  int leSommai = uneTaille;//variable pour le sommai
+  for (int leI = 0; leI < uneTaille; leI++)//boucle de hauteur
   {
-    for (int leJ = 0; leJ < unEspace; leJ++)
-    {
-      std::cout << " ";
-    }
-
+    dessinerEspace(unEspace);//fonction d'espace
     for (int leJ = 0; leJ < leSommai; leJ++)
     {
       if (unRempl == "fill")
@@ -125,28 +123,27 @@ void dessinerTriangleRectangleInv(int uneTaille, int unEspace, char unCar, std::
         }
       }
     }
-    std::cout <<'\n';
-    leSommai--;
+    std::cout <<'\n';//retour a la ligne
+    leSommai--;//décrémentation du sommai
   }
 }
 
+// fonction qui dessine un triangle
+//elle prend les même argument que dessinerCarre
 void dessinerTriangle(int uneTaille, int unEspace, char unCar, std::string unRempl)
 {
-  for (int leI = 0; leI < (uneTaille / 2) + (uneTaille % 2); leI++)
+  for (int leI = 0; leI < (uneTaille / 2) + (uneTaille % 2); leI++)//boucle de hauteur avec condition pour trouvée la hauteur max
   {
-    for (int leJ = 0; leJ < (unEspace + leI); leJ++)
+    dessinerEspace(unEspace + leI);//fonction d'espace avec imcrément de l'espace
+    for (int leJ = 0; leJ < (uneTaille - leI*2) ; leJ++)//boucle de ligne utilisation du leI pour avoir la pointe du triangle
     {
-      std::cout << " ";
-    }
-    for (int leJ = 0; leJ < (uneTaille - leI*2) ; leJ++)
-    {
-      if (unRempl == "fill")
+      if (unRempl == "fill")//condition remplissage
       {
         std::cout << unCar;
       }
       else
       {
-        if ((leJ == 0 ) || leJ == ((uneTaille - 1) - (leI * 2)) || leI == 0 )
+        if ((leJ == 0 ) || leJ == ((uneTaille - 1) - (leI * 2)) || leI == 0 )//condition contour
         {
           std::cout << unCar;
         }
@@ -156,19 +153,18 @@ void dessinerTriangle(int uneTaille, int unEspace, char unCar, std::string unRem
         }
       }
     }
-    std::cout << '\n';
+    std::cout << '\n';//retour a la ligne
   }
 }
 
-
-void dessinerTriangleInverse(int uneTaille, int unEspace, char unCar, std::string unRempl)
+// fonction qui dessine un triangle inverser
+//elle prend les même argument que dessinerCarre
+//fonctionne comme dessinerTriangle mais en décrémentation
+void dessinerTriangleInv(int uneTaille, int unEspace, char unCar, std::string unRempl)
 {
-  for (int leI = (uneTaille / 2) + (uneTaille % 2); leI > -1; leI--)
+  for (int leI = (uneTaille / 2) + (uneTaille % 2); leI > -1; leI--)//
   {
-    for (int leJ = 0; leJ < (unEspace + leI); leJ++)
-    {
-      std::cout << " ";
-    }
+    dessinerEspace(unEspace + leI);
     for (int leJ = 0; leJ < (uneTaille - leI*2) ; leJ++)
     {
       if (unRempl == "fill")
@@ -191,16 +187,16 @@ void dessinerTriangleInverse(int uneTaille, int unEspace, char unCar, std::strin
   }
 }
 
+// fonction qui dessine un losange
+//elle prend les même argument que dessinerCarre
 void dessinerLosange(int uneTaille, int unEspace, char unCar, std::string unRempl)
 {
-  int leTabTest[2] = {uneTaille/2, uneTaille%2};
-  bool leMilieur = false;
-  for (int leI = 0; leI < uneTaille; leI++) {
-    for (int leJ = 0; leJ < unEspace+leTabTest[0]; leJ++) {
-      std::cout << " ";
-    }
-    for (int leJ = 0; leJ < leTabTest[1]; leJ++) {
-      if (unRempl == "fill") {
+  int leTabTest[2] = {uneTaille/2, uneTaille%2};//petit tableau (pas obligatoire) pour savoir ou j'en suis dans le dessin de mon losange
+  bool leMilieur = false;//variable conditionnel pour savoire quand on est au milieur du losange
+  for (int leI = 0; leI < uneTaille; leI++) {//boucle pour la hauteur
+    dessinerEspace(unEspace+leTabTest[0]);//fonction pour les espace
+    for (int leJ = 0; leJ < leTabTest[1]; leJ++) {//fonction pour les lignes
+      if (unRempl == "fill") {//contition pour le remplissage
         if (leJ == 0 || leJ == leTabTest[1]-1) {
           std::cout << unCar;
         } else {
@@ -211,25 +207,26 @@ void dessinerLosange(int uneTaille, int unEspace, char unCar, std::string unRemp
       }
     }
     std::cout << '\n';
-    if (leMilieur == false) {
-      leTabTest[0] --;
-      leTabTest[1] += 2;
+    if (leMilieur == false) {//condition d'incrément /décrément pour les pointes
+      leTabTest[0] --;//décrément de l'espace
+      leTabTest[1] += 2;//incément du nombre de caratère
     }
     else
     {
-      leTabTest[0]++;
-      leTabTest[1] -= 2;
+      leTabTest[0]++;//incrément de l'espace
+      leTabTest[1] -= 2;//décrément du nombre de caratère
     }
-    if (leTabTest[0] == 0) {
-      leMilieur = true;
+    if (leTabTest[0] == 0) {//détéction du milieu
+      leMilieur = true;//milieur passer passe donc a true
     }
   }
 }
 
-
+//petite fonction qui renvoie le numéro de la forma a faire
 int decoderForme(std::string uneForme)
 {
   std::string lesFormePossible[6]={"carre","triangle rectangle","triangle rectangle inverse","triangle","triangle inverse","losange"};
+  //tableau avec toutes les forme disponible
   for (int leI = 0; leI < 6; leI++)
   {
     if (uneForme == lesFormePossible[leI])
@@ -237,17 +234,17 @@ int decoderForme(std::string uneForme)
       return leI;
     }
   }
-  return -1;
+  return -1;//si aucune forme ne corespond a la valeur entrée
 }
 
 
 int main()
 {
-  std::ifstream monFichier("figure.txt");
+  std::ifstream monFichier("figure.txt");//ouverture du fichier
   std::string laForme, leRemplissage;
   int laTaille, laDecal;
   char leCar;
-  while (not monFichier.eof())
+  while (not monFichier.eof())//lecture du fichier
   {
     getline(monFichier, laForme);
     monFichier >> laTaille;
@@ -261,7 +258,8 @@ int main()
 
     getline(monFichier, leRemplissage);
 
-    switch (decoderForme(laForme)) {
+    switch (decoderForme(laForme)) {//utilisation de la fonction decoderForme pour savoir a quel forme on a faire
+    //petit switch pour sélection la bonne fonction suivant la forme voulu.
       case 0:
         dessinerCarre(laTaille, laDecal, leCar, leRemplissage);
       break;
@@ -279,7 +277,7 @@ int main()
       break;
 
       case 4:
-        dessinerTriangleInverse(laTaille, laDecal, leCar, leRemplissage);
+        dessinerTriangleInv(laTaille, laDecal, leCar, leRemplissage);
       break;
 
       case 5:
@@ -287,8 +285,9 @@ int main()
       break;
 
       default :
-      std::cout << "ERROR ! \n forme non pris en charge" << '\n';
-    }
+      std::cout << "ERREUR ! \n forme non pris en charge" << '\n';//retoure d'érreur si forme non reconus
+    }//PS : le retourne a la ligne de fin de fichier et reconnus comme une forme inconu d'ou l'erreur en fin de programme
   }
-  return 0;
+  monFichier.close();//fermeture du fichier
+  return 0;//fin de programme
 }
